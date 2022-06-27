c88bdaed045d194599ec03b7588581f7d9de6349
RaoHamza6 <97746122+RaoHamza6@users.noreply.github.com>
2022-06-27 10:15:20 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['Tazeem']
+global.premium = ['+971564783202']
+global.ownernomer = '+971564783202'
+global.ownername = 'Zakhmi Dil'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
